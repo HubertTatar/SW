aea1f276b07a1922af6f29945fe24d01cca0a511
HubertTatar <hutatar@gmail.com>
2018-03-02 22:38:29 +0100

    test

--- src/test/java/io/huta/application/ServerTest.java
+++ src/test/java/io/huta/application/ServerTest.java
@@ -1,4 +1,40 @@
 package io.huta.application;
 
-public class ServerTest {
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.web.reactive.function.client.ClientResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+class ServerTest {
+
+    private Server server = new Server(8080);
+    private WebClient webClient = WebClient.create("http://localhost:8080");
+
+
+    @BeforeAll
+    void beforeAll() {
+        server.start();
+    }
+
+    @Test
+    void shouldGet200() {
+        webClient
+                .get()
+                .uri("/hi")
+                .exchange()
+                .map(ClientResponse::statusCode)
+                .block()
+                .is2xxSuccessful();
+    }
+
+
+    @AfterAll
+    void afterAll() {
+        server.stop();
+    }
+
+
 }
